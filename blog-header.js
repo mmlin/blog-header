7edb4b0fa294ebf64c24adbb5173d61fa200d994
Mike M. Lin <mike.m.lin@gmail.com>
2015-08-24 16:31:38 -0700

    Initial commit

---
+++ blog-header.js
@@ -0,0 +1,33 @@
+function initHeader() {
+  // Hide the original header and show the new one.
+  $('#site-header').hide();
+  $('.blog-header').show();
+
+  // Set the active subnavigation item.
+  switch (window.location.pathname) {
+  case '/infographics/':
+    $('.bh-infographics').addClass('active');
+    break;
+
+  case '/marketing-guides/':
+    $('.bh-guides').addClass('active');
+    break;
+
+  case '/webinars/':
+    $('.bh-webinars').addClass('active');
+    break;
+
+  case '/topics/academy/':
+    $('.bh-academy').addClass('active');
+    break;
+
+  default:
+    $('.bh-blog').addClass('active');
+  }
+
+  // Enable open/close of the mobile menu.
+  $('.bh-mobl-icon').click(function() {
+    $(this).toggleClass('open');
+    $('.bh-nav-mobl').toggleClass('open');
+  });
+}
